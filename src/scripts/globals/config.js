0922217b06d9dc9251a58b9bdc46790325dfa310
TotonAntoko <Totonantoko@gmail.com>
2021-05-24 10:46:42 +0700

    Update config.js
    add image base url

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,10 @@
 const CONFIG = {
-  BASE_URL: 'https://restaurant-api.dicoding.dev/'
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  IMAGE_BASE_URL: {
+    SMALL: 'https://restaurant-api.dicoding.dev/images/small/',
+    MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
+    LARGE: 'https://restaurant-api.dicoding.dev/images/large/'
+  }
 }
 
 export default CONFIG
