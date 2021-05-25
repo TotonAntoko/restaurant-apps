f0b0a1d7575bab8eaa6daecbc238a7f7fff69d74
TotonAntoko <Totonantoko@gmail.com>
2021-05-26 06:57:18 +0700

    Update api-endpoint.js
    add end-point detail restaurant

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,7 +1,10 @@
 import CONFIG from './config'
 
 const API_ENDPOINT = {
-  RESTAURANT: { LIST: `${CONFIG.BASE_URL}list` }
+  RESTAURANT: {
+    LIST: `${CONFIG.BASE_URL}list`,
+    DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`
+  }
 }
 
 export default API_ENDPOINT
