fbea538253b3556694775eaba54a4524af16cb10
TotonAntoko <Totonantoko@gmail.com>
2021-05-27 12:32:03 +0700

    add page favorite restaurant

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,9 +1,25 @@
+// components
+import '../../component/card-restaurant'
+
 const Favorite = {
   async render () {
     return `
-      <h1>This Page Favorite</h1>
+      <div class="container">
+        <div class="main-list" style="margin-top: 80px;">
+          <div class="main-header">
+            <div class="main-title">
+              <h1>Favorited Restaurant</h1>
+            </div>
+          </div>
+          <div class="main-content">
+            <card-restaurant></card-restaurant>
+          </div>
+        </div>
+      </div>
     `
-  }
+  },
+
+  async afterRender () {}
 }
 
 export default Favorite
