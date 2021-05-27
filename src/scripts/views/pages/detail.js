c49e6679c90b3e75ab13e1b3fb9696c9c8764b2f
TotonAntoko <Totonantoko@gmail.com>
2021-05-27 11:44:41 +0700

    show list review restaurant

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,7 +1,7 @@
 import UrlParser from '../../routes/url-parser'
 import RestaurantSource from '../../data/restaurant-source'
 
-import { createRestaurantMenusTemplate } from '../templates/template-creator'
+import { createRestaurantMenusTemplate, createCustomerReviewTemplate } from '../templates/template-creator'
 
 import '../../component/restaurant-detail-header'
 
@@ -13,6 +13,25 @@ const Detail = {
       </div>
       <div class="container">
         <div class="main-menu"></div>
+        <div class="main-list" style="margin-top: 40px;">
+          <div class="main-header">
+            <div class="main-title">
+              <h1>Review</h1>
+            </div>
+          </div>
+          <form class="main-form">
+            <div class="col-form">
+              <label for="name">Name</label>
+              <input type="text" id="name" name="name" placeholder="Enter your name" value="">
+            </div>
+            <div class="col-form">
+              <label for="review">Review</label>
+              <textarea id="review" name="review" placeholder="Your review here..." rows="5"></textarea>
+            </div>
+            <button id="submitReview" aria-label="submit review" class="btn btn-warning">Submit</button>
+          </form>
+          <div class="main-review"></div>
+        </div>
       </div>
     `
   },
@@ -26,6 +45,9 @@ const Detail = {
 
     // Get Menu Categories
     Object.keys(restaurant.menus).map((category) => $('.main-menu').append(createRestaurantMenusTemplate(category, restaurant.menus)))
+
+    // Get Customer Review
+    restaurant.customerReviews.map((review) => $('.main-review').append(createCustomerReviewTemplate(review)))
   }
 }
 
