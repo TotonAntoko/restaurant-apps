5b3d55af2bf710407db9a321f2cae99c982eca93
TotonAntoko <Totonantoko@gmail.com>
2021-05-28 08:01:05 +0700

    add end point review customer

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -3,7 +3,8 @@ import CONFIG from './config'
 const API_ENDPOINT = {
   RESTAURANT: {
     LIST: `${CONFIG.BASE_URL}list`,
-    DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`
+    DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
+    REVIEW: `${CONFIG.BASE_URL}review`
   }
 }
 
